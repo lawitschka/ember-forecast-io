592d40a73a9aa4617dcf03179e25ba1ff2ea4836
Volodymyr Radchenko <30495731+rreckonerr@users.noreply.github.com>
2020-08-12 23:38:41 +0300

    CHORE: upgrade addon to ember v 3.12 (#2)
    * ENH: add yarn.lock file
    
    Having a .lock file is generally a good idea :)
    Also, without it I have to manually set "resolutions" for ember-forecast-io/ember-data in the consuming app.
    
    * ENH: upgrade to ember 3.12
    
    * ENH: run ember-qunit-codemod
    
    To update the test API syntax
    
    * ENH: add lint and js configuration
    
    * ENH: convert addon to native classes
    
    * FIX: async model relationships
    
    After upgrading ember-forecast-io we started receiving errors related to requests made for fetching `data_points`, which in fact are supposed to be embedded. According to EmbeddedRecordsMixin documentation `If a model is declared embedded for deserialization (embedded: 'always' or deserialize: 'records'), then do not use async: true`.  And { async: true } is the default option for relationships.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,19 +2,55 @@ module.exports = {
   root: true,
   parser: 'babel-eslint',
   parserOptions: {
-    ecmaVersion: 7,
-    sourceType: 'module'
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: { 
+      legacyDecorators: true 
+    }
   },
+  plugins: ['ember'],
   extends: [
-    'plugin:ember-suave/recommended'
+    'plugin:ember/recommended'
   ],
   env: {
     'browser': true
   },
   rules: {
+    'ember/use-ember-data-rfc-395-imports': false,
     'strict': 0,
-    'ember-suave/require-access-in-comments': 0,
     'no-unused-vars': 'error',
     'no-dupe-keys': 'error'
-  }
+  },
+  overrides: [
+    // node files
+    {
+      files: [
+        '.eslintrc.js',
+        '.template-lintrc.js',
+        'ember-cli-build.js',
+        'index.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js'
+      ],
+      excludedFiles: [
+        'addon/**',
+        'addon-test-support/**',
+        'app/**',
+        'tests/dummy/app/**'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      },
+      env: {
+        browser: false,
+        node: true
+      },
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
+      })
+    }
+  ]
 };
